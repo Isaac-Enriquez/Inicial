259acb4ec34c9a75c84b9c7c359dc3b6449de7b0
Isaac-Enriquez <a00829207@itesm.mx>
2020-10-26 16:29:39 -0600

    Update Inicial.py

--- Inicial.py
+++ Inicial.py
@@ -3,10 +3,13 @@
 
 #Este programa calcula el área
 #y perímetro de un rectángulo.
-
+def calcular_perimetro(lado1, lado2):
+    perimetro = 2*lado1 + 2*lado2
+    return perimetro
 #funcion1  calcular área del rectángulo
-
-
+def calcular_area(lado1, lado2):
+    area = lado1*lado2
+    return area
 #funcion2  calcular perímetro del rectángulo
 
 
@@ -19,6 +22,6 @@ print("medida de lado 2 del rectángulo")
 l2 = float(input())
 
 #desplegar calculo funcion1
-
+print(calcular_area(l1, l2))
 #desplegar calculo funcion 2
-
+print(calcular_perimetro(l1,l2))
